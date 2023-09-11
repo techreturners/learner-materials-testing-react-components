52de7f179b36ee7c4adff841382aa617f9674847
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-11 23:33:07 +0100

    Select WhatIs2+2

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -3,6 +3,7 @@ import W12MHeader from "./W12MHeader";
 import W12MInputSpeciesName from "./W12MInputSpeciesName";
 import W12MInputPlanetName from "./W12MInputPlanetName";
 import W12MInputNumberOfBeings from "./W12MInputNumberOfBeings";
+import W12MInputWhatIs from "./W12MInputWhatIs";
 
 const W12MForm = () => {
   return (
@@ -11,6 +12,7 @@ const W12MForm = () => {
       <W12MInputSpeciesName />
       <W12MInputPlanetName />
       <W12MInputNumberOfBeings />
+	  <W12MInputWhatIs />
     </section>
   );
 };
---
+++ src/components/W12MInputWhatIs.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const W12MInputWhatIs = () => {
+  const [whatIs, setWhatIs] = useState("");
+
+  const handleWhatIsChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setWhatIs(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="whatIs">What is 2+2?</label>
+      <select id="whatIs" value={whatIs} onChange={handleWhatIsChange}>
+        <option value="4">4</option>
+        <option value="Not 4">Not 4</option>
+      </select>
+    </div>
+  );
+};
+
+export default W12MInputWhatIs;
