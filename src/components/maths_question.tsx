d5dffa4fd7977f33115419e731365acacd478ca8
netcam22 <annettelesage@btinternet.com>
2023-12-13 09:09:57 +0000

    feat: add name to select

--- src/components/maths_question.tsx
+++ src/components/maths_question.tsx
@@ -7,7 +7,7 @@ interface MathsQuestionProps {
 export const MathsQuestion : React.FC<MathsQuestionProps> = ({mathsAnswer, onChangeMathsAnswer}) => (
     <>
         <label htmlFor='mathsQuestion'>What is 2 + 2?</label>
-        <select value={mathsAnswer} onChange={e => onChangeMathsAnswer(e.target.value)}>
+        <select name="mathsQuestion" value={mathsAnswer} onChange={e => onChangeMathsAnswer(e.target.value)}>
 			<option value="not4">Not 4</option>
 			<option value="0">0</option>
 			<option value="4">4</option>
