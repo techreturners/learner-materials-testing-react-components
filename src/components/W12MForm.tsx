aa58551ab920bb730122ff2f72f1e9ebf138475f
netcam22 <annettelesage@btinternet.com>
2023-12-13 08:08:49 +0000

    feat: add planet name field to form

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react';
 import W12MHeader from './W12MHeader';
 import {SpeciesName} from './species_name';
+import { PlanetName } from './planet_name';
 
 const W12MForm = () => {
 	const [speciesName, setSpeciesName] = useState('humans');
+	const [planetName, setPlanetName] = useState('earth');
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
 			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} />
-			{/* REST OF FORM GOES HERE */}
+			<PlanetName planetName={planetName} onChangePlanetName={(value) => setPlanetName(value)} />
 		</section>
 	);
 };
