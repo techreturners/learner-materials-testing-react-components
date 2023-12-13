1a14e77fbcdc72a8b272b88a47383936444f8cb9
netcam22 <annettelesage@btinternet.com>
2023-12-13 08:13:47 +0000

    feat: add number of beings field to form

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -2,15 +2,19 @@ import { useState } from 'react';
 import W12MHeader from './W12MHeader';
 import {SpeciesName} from './species_name';
 import { PlanetName } from './planet_name';
+import { NumberOfBeings } from './number_of_beings';
 
 const W12MForm = () => {
 	const [speciesName, setSpeciesName] = useState('humans');
 	const [planetName, setPlanetName] = useState('earth');
+	const [numberOfBeings, setNumberOfBeings] = useState('population');
+
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
 			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(value) => setSpeciesName(value)} />
 			<PlanetName planetName={planetName} onChangePlanetName={(value) => setPlanetName(value)} />
+			<NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(value) => setNumberOfBeings(value)} />
 		</section>
 	);
 };
