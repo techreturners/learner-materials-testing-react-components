82063b7a99a3a17b106a07920602b08bf346f043
netcam22 <annettelesage@btinternet.com>
2023-12-16 19:23:06 +0000

    feat: implement tests for validation function

--- src/validate/validate_input.test.tsx
+++ src/validate/validate_input.test.tsx
@@ -29,7 +29,7 @@ describe("test validation of user input", () => {
 		expect(validationCall).toEqual([params.message]);
 	});	
 
-	test('Species Name returns error message if input is invalid', () => {
+	test('Species Name validation returns error message if input is invalid', () => {
 		//Arrange
 		const params = {
 			title: "Species Name",
@@ -55,5 +55,47 @@ describe("test validation of user input", () => {
 		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
 		//Assert
 		expect(validationCall).toEqual([]);
+	});	
+
+	test('Number of Beings validation returns error message if input is invalid', () => {
+		//Arrange
+		const params = {
+			title: "Number of Beings",
+			regex: /^[0-9]{10,}$/g,
+			value: "999999999",
+			message: "Numbers ONLY. Must be at least 1,000,000,000"
+		};
+		//Act
+		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
+		//Assert
+		expect(validationCall).toEqual([params.message]);
+	});	
+
+	test('Number of Beings validation returns empty array if input is valid', () => {
+		//Arrange
+		const params = {
+			title: "Number of Beings",
+			regex: /^[0-9]{10,}$/g,
+			value: "9999999999",
+			message: "Numbers ONLY. Must be at least 1,000,000,000"
+		};
+		//Act
+		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
+		//Assert
+		expect(validationCall).toEqual([]);
+	});	
+
+	test('Maths questions validation returns error message if selected option is incorrect', () => {
+		//Arrange
+		const params = {
+			title: "What is 2 + 2?",
+			regex: /^4{1}$/,
+			value: "4 million",
+			message: `"4" must be selected`
+		};
+		//Act
+		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
+		//Assert
+		expect(validationCall).toEqual([params.message]);
 	});	
 });
