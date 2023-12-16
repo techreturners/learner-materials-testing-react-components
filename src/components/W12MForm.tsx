61f3c269fc8215426eca8fef3be375b6284edac7
netcam22 <annettelesage@btinternet.com>
2023-12-16 09:57:58 +0000

    update form input for reusability

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -9,15 +9,19 @@ import { SubmitButton } from './submit_button';
 import { formInput} from '../data/alien_form_data';
 import { validateTextInput } from "../validate/validate_text_input"
 
+type InitialValue = {[key: string]: string};
 const W12MForm = () => {
 	
-	const initialValues = {
+	const initialValues: InitialValue = {
 		speciesName: "", 
 		planetName: "", 
 		numberOfBeings: "",
 		mathsAnswer: "",
 		reasonsForSparing: ""
 	}
+
+	const keys = Object.keys(initialValues);
+
 	const [input, setInput] = useState({...initialValues});
 	const [submitted, setSubmitted] = useState(false);
 
@@ -50,17 +54,17 @@ const W12MForm = () => {
 			<div className = "col-50-left">
 			<TextInput title = {formInput[0].title} role = {formInput[0].role} 
 			regex={formInput[0].regex} message = {formInput[0].errorMessage}
-			value={input.speciesName} onChange={handleChange} submitted={submitted}
+			value={input[keys[0]]} onChange={handleChange} submitted={submitted}
 			validate = {validateTextField}/>
 
 			<TextInput title = {formInput[1].title} role = {formInput[1].role} 
 			regex={formInput[1].regex} message = {formInput[1].errorMessage} 
-			value={input.planetName} onChange={handleChange} submitted={submitted}
+			value={input[keys[1]]} onChange={handleChange} submitted={submitted}
 			validate = {validateTextField}/>
 
 			<TextInput title = {formInput[2].title} role = {formInput[2].role} 
 			regex={formInput[2].regex} message = {formInput[2].errorMessage} 
-			value={input.numberOfBeings} onChange={handleChange} submitted={submitted}
+			value={input[keys[2]]} onChange={handleChange} submitted={submitted}
 			validate = {validateTextField}/>
 
 			<MathsQuestion mathsAnswer={input.mathsAnswer} onChangeMathsAnswer={handleChange} />
@@ -70,20 +74,20 @@ const W12MForm = () => {
 			<SubmitButton buttonText = "Submit Application" onSubmitHandler = {handleSubmit}/>
 
 			</div>
-			<Output title = {formInput[0].title} value = {input.speciesName} message = {formInput[0].errorMessage}
+			<Output title = {formInput[0].title} value = {input[keys[0]]} message = {formInput[0].errorMessage}
 			role = {formInput[0].role} regex={formInput[0].regex} validate = {validateTextField} submitted={submitted}/>
 
-			<Output title = {formInput[1].title} value = {input.planetName} message = {formInput[1].errorMessage}
-			role = {formInput[0].role} regex={formInput[1].regex} validate = {validateTextField} submitted={submitted}/>
+			<Output title = {formInput[1].title} value = {input[keys[1]]} message = {formInput[1].errorMessage}
+			role = {formInput[1].role} regex={formInput[1].regex} validate = {validateTextField} submitted={submitted}/>
 
-			<Output title = {formInput[2].title} value = {input.numberOfBeings} message = {formInput[2].errorMessage}
-			role = {formInput[0].role} regex={formInput[2].regex} validate = {validateTextField} submitted={submitted}/>
+			<Output title = {formInput[2].title} value = {input[keys[2]]} message = {formInput[2].errorMessage}
+			role = {formInput[2].role} regex={formInput[2].regex} validate = {validateTextField} submitted={submitted}/>
 
-			<Output title = {formInput[3].title} value = {input.mathsAnswer} message = {formInput[3].errorMessage}
-			role = {formInput[0].role} regex={formInput[3].regex} validate = {validateTextField} submitted={submitted}/>
+			<Output title = {formInput[3].title} value = {input[keys[3]]} message = {formInput[3].errorMessage}
+			role = {formInput[3].role} regex={formInput[3].regex} validate = {validateTextField} submitted={submitted}/>
 			
-			<Output title = {formInput[4].title} value= {input.reasonsForSparing} message = {formInput[4].errorMessage}
-			role = {formInput[0].role} regex={formInput[4].regex} validate = {validateTextField} submitted={submitted}/>
+			<Output title = {formInput[4].title} value= {input[keys[4]]} message = {formInput[4].errorMessage}
+			role = {formInput[4].role} regex={formInput[4].regex} validate = {validateTextField} submitted={submitted}/>
 		</section>	
 	);
 };
