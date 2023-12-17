f11581ee426e012ee15bb82269e899626775b421
netcam22 <annettelesage@btinternet.com>
2023-12-17 10:25:44 +0000

    refactor: update tests for arrays of error message values

--- src/components/error_message.test.tsx
+++ src/components/error_message.test.tsx
@@ -1,30 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import { ErrorMessage, ErrorMessageProps } from './error_message';
 
-test('Error message Must be between 17 and 153... is displayed when passed in through props', () => {
+test('Error message must be between 17 and 153... is displayed when passed in through props', () => {
 
 	const requiredProps: ErrorMessageProps = {
-		message: "Must be between 17 and 153 characters"
+		message: "must be between 17 and 153 characters"
 	}
 
 	render(<ErrorMessage {...requiredProps}/>);
 	
 	const text = screen.getByText(
-		/Must be between 17 and 153 characters/i
+		/must be between 17 and 153 characters/i
 	);
 	expect(text).toBeInTheDocument();
 });
 
-test('Error Numbers ONLY... is displayed when passed in through props', () => {
+test('Error numbers ONLY... is displayed when passed in through props', () => {
 
 	const requiredProps: ErrorMessageProps = {
-		message: "Numbers ONLY. Must be at least 1,000,000,000"
+		message: "numbers ONLY; must be at least 1,000,000,000"
 	}
 
 	render(<ErrorMessage {...requiredProps}/>);
 	
 	const text = screen.getByText(
-		/Numbers ONLY. Must be at least 1,000,000,000/i
+		/numbers ONLY; must be at least 1,000,000,000/i
 	);
 	expect(text).toBeInTheDocument();
 });
--- src/validate/validate_input.test.tsx
+++ src/validate/validate_input.test.tsx
@@ -33,23 +33,23 @@ describe("test validation of user input", () => {
 		//Arrange
 		const params = {
 			title: "Species Name",
-			regex: [/^[a-z]{3,23}$/gi],
+			regex: [/^.{3,23}$/gi, /^[a-z]+$/gi],
 			value: "F%",
-			message: ["Must be between 3 and 23 characters. No numbers or special characters allowed!"]
+			message: ["must be between 3 and 23 characters", "no numbers or special characters allowed"]
 		};
 		//Act
 		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
 		//Assert
-		expect(validationCall).toEqual([params.message[0]]);
+		expect(validationCall).toEqual(["must be between 3 and 23 characters", "no numbers or special characters allowed"]);
 	});	
 
 	test('Species Name validation returns empty array if input is valid', () => {
 		//Arrange
 		const params = {
 			title: "Species Name",
-			regex: [/^[a-z]{3,23}$/gi],
+			regex:  [/^.{3,23}$/gi, /^[a-z]+$/gi],
 			value: "Fred",
-			message: ["Must be between 3 and 23 characters. No numbers or special characters allowed!"]
+			message: ["must be between 3 and 23 characters", "no numbers or special characters allowed"]
 		};
 		//Act
 		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
@@ -57,27 +57,55 @@ describe("test validation of user input", () => {
 		expect(validationCall).toEqual([]);
 	});	
 
-	test('Number of Beings validation returns error message if input is invalid', () => {
+	test('Species Name validation returns single message if 3 or more number or special characters', () => {
 		//Arrange
 		const params = {
-			title: "Number of Beings",
-			regex: [/^[0-9]{10,}$/g],
-			value: "999999999",
-			message: ["Numbers ONLY. Must be at least 1,000,000,000"]
+			title: "Species Name",
+			regex:  [/^.{3,23}$/gi, /^[a-z]+$/gi],
+			value: "5&%$",
+			message: ["must be between 3 and 23 characters", "no numbers or special characters allowed"]
 		};
 		//Act
 		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
 		//Assert
-		expect(validationCall).toEqual([params.message[0]]);
+		expect(validationCall).toEqual(["no numbers or special characters allowed"]);
+	});	
+
+	test('Species Name validation returns single message if fewer than 3 valid characters', () => {
+		//Arrange
+		const params = {
+			title: "Species Name",
+			regex:  [/^.{3,23}$/gi, /^[a-z]+$/gi],
+			value: "An",
+			message: ["must be between 3 and 23 characters", "no numbers or special characters allowed"]
+		};
+		//Act
+		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
+		//Assert
+		expect(validationCall).toEqual(["must be between 3 and 23 characters"]);
+	});	
+
+	test('Number of Beings validation returns error message if input is invalid', () => {
+		//Arrange
+		const params = {
+			title: "Number of Beings",
+			regex: [/^[0-9]+$/g, /^[0-9]{10,}$/g],
+			value: "999999999",
+			message: ["numbers ONLY", "must be at least 1,000,000,000"]
+		};
+		//Act
+		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
+		//Assert
+		expect(validationCall).toEqual(["must be at least 1,000,000,000"]);
 	});	
 
 	test('Number of Beings validation returns empty array if input is valid', () => {
 		//Arrange
 		const params = {
 			title: "Number of Beings",
-			regex: [/^[0-9]{10,}$/g],
+			regex: [/^[0-9]+$/g, /^[0-9]{10,}$/g],
 			value: "9999999999",
-			message: ["Numbers ONLY. Must be at least 1,000,000,000"]
+			message: ["numbers ONLY", "must be at least 1,000,000,000"]
 		};
 		//Act
 		const validationCall = validateInput(params.title, params.regex, params.value, params.message);
